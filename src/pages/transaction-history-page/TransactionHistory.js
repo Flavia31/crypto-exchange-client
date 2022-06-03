2bd793b6a53b3c368558f903374e605897868c6b
Alexandra Ciui <62381627+Flavia31@users.noreply.github.com>
2022-06-03 14:56:05 +0300

    Update TransactionHistory.js

--- src/pages/transaction-history-page/TransactionHistory.js
+++ src/pages/transaction-history-page/TransactionHistory.js
@@ -1,8 +1,6 @@
 import { Typography } from "@mui/material";
 
 export function TransactionHistoryPage() {
-
-
     return (
       <main className="transaction-history-page">
         <Typography variant="h2" className="transaction-history-title">Transaction History</Typography>
