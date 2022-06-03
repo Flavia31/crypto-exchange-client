a3e6cd51040b4d9afc87d38f756088998ea75ee7
Flavia31 <ciui.alexandra31@gmail.com>
2022-06-03 13:18:47 +0300

    [FE] Create Login Page: add useState in LoginPage.js

--- src/services/requests.js
+++ src/services/requests.js
@@ -8,29 +8,20 @@ const routes = {
     google: "auth/google"
 }
 
-  const headers = {
-     'Access-Control-Allow-Origin' : '*',
-     'Access-Control-Allow-Methods':'GET, POST, OPTIONS, PUT, DELETE',
-     'Access-Control-Request-Headers': 'Content-Type, Authorization ',
-     'Content-Type': 'application/json',
-     'Cache-Control': null,
-    'X-Requested-With': null,
-   }
-
-const client = axios.create({
-    baseURL: `${constants.constants.baseURL}`,
-    headers: headers,
-    method: 'GET'
-  });
-
 
 export const get = (routeKey) => {
-    return client.get(`/${routes[routeKey]}`).then(function(response){
+    return axios.get(`${constants.constants.baseURL}/${routes[routeKey]}`).then(function(response){
         //Do something with response
         console.log(response);
     });
 }
 
 export const post = (routeKey, data) => {
-    return client.post( `${routes[routeKey]}`, data)
+    return axios.post( `${constants.constants.baseURL}/${routes[routeKey]}`, data, ).then(function(response){
+        //Do something with response
+        console.log(response);
+    })
+    .catch(error => {
+        console.error(error)
+    })
 }
